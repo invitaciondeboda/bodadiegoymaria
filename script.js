1b650c72b1f5432b340b3abcf23413cb1718a640
invitaciondeboda <noscasamos.invitacion@gmail.com>
2025-04-21 19:42:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -31,22 +31,7 @@ function updateCountdown() {
     <div class="cuenta-item">
       <div class="numero">${seconds}</div>
       <div class="unidad">SEG</div>
-    </div>
-  `;
+    </div>`;
 }
 
 setInterval(updateCountdown, 1000);
-
-// Scroll animation para los elementos del timeline
-const items = document.querySelectorAll('.timeline-item');
-const observer = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add('visible');
-    }
-  });
-}, {
-  threshold: 0.6
-});
-
-items.forEach(item => observer.observe(item));
